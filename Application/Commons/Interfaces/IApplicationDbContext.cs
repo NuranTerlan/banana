35ed3164a0e1631bada0a67100030a96b52b9730
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-30 01:17:54 +0400

    Update IApplicationDbContext.cs

--- Application/Commons/Interfaces/IApplicationDbContext.cs
+++ Application/Commons/Interfaces/IApplicationDbContext.cs
@@ -1,7 +1,16 @@
-﻿namespace Application.Commons.Interfaces
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Commons.Interfaces
 {
     public interface IApplicationDbContext
     {
-        
+        DbSet<Library> Libraries { get; set; }
+        DbSet<Book> Books { get; set; }
+        DbSet<Author> Authors { get; set; }
+        DbSet<Category> Categories { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
 }
