d4eb0c08b879cade9b3db1b30d9d7c53f8b0ccb2
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-29 23:52:51 +0400

    Create Category.cs

---
+++ Domain/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Commons;
+
+namespace Domain.Entities
+{
+    public class Category : AuditEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
