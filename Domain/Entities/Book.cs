8b25206dbd1ee963719cc8e5fd99b94463843bcf
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-29 23:52:48 +0400

    Create Book.cs

---
+++ Domain/Entities/Book.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Commons;
+
+namespace Domain.Entities
+{
+    public class Book : AuditEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public int LibraryId { get; set; }
+        public Library Library { get; set; }
+    }
+}
