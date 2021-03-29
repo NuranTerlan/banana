ea9195ea4439d257b56cf8918a8e8db1898703ee
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-29 23:52:54 +0400

    Create Library.cs

---
+++ Domain/Entities/Library.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Domain.Commons;
+
+namespace Domain.Entities
+{
+    public class Library : AuditEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public IList<Book> Books { get; set; }
+    }
+}
