81b5cb272ee0d81c6cc583ce5e6b904b11bfbd73
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-29 23:52:30 +0400

    audit or base entity for all domains

---
+++ Domain/Commons/AuditEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Domain.Commons
+{
+    public class AuditEntity
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreationTime { get; set; }
+        public DateTime? LastModificationTime { get; set; }
+    }
+}
