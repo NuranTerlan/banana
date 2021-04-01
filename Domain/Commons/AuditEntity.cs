7c9486796963e7d2234d1e01c87aa2e82aef921d
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-01 22:07:43 +0400

    Update AuditEntity.cs

--- Domain/Commons/AuditEntity.cs
+++ Domain/Commons/AuditEntity.cs
@@ -4,6 +4,7 @@ namespace Domain.Commons
 {
     public class AuditEntity
     {
+        public int Id { get; set; }
         public string CreatedBy { get; set; }
         public DateTime CreationTime { get; set; }
         public DateTime? LastModificationTime { get; set; }
