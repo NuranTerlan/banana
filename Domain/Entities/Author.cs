37ef9f46c89fc55f9a59d40c397cc85ddf3560cf
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-01 22:07:48 +0400

    Update Author.cs

--- Domain/Entities/Author.cs
+++ Domain/Entities/Author.cs
@@ -4,7 +4,6 @@ namespace Domain.Entities
 {
     public class Author : AuditEntity
     {
-        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Biography { get; set; }
