21747ae41c2b22d0864a519f5a14da85b2f035dc
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-01 22:08:03 +0400

    Update DependencyInjection.cs

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,6 +1,5 @@
 ﻿using Application.Commons.Interfaces;
 using Infrastructure.Data;
-using Infrastructure.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
