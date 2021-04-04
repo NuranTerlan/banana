bd88694c46d972427fb0e0c239fb3b4662a8656c
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-04 23:33:36 +0400

    mediatR request-handler wrapper is prepared

---
+++ Application/Wrappers/IHandlerWrapper.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Application.Wrappers
+{
+    public interface IHandlerWrapper<in TIn, TOut> : IRequestHandler<TIn, Response<TOut>>
+        where TIn : IRequestWrapper<TOut>
+    {
+        
+    }
+}
