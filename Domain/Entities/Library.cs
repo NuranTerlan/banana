5b6d0bedab048588a06e3d3a5447d80e2ce9543f
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-05 22:06:57 +0400

    Update Library.cs

--- Domain/Entities/Library.cs
+++ Domain/Entities/Library.cs
@@ -5,11 +5,6 @@ namespace Domain.Entities
 {
     public class Library : AuditEntity
     {
-        public Library()
-        {
-            Books = new List<Book>();
-        }
-
         public string Name { get; set; }
 
         public ICollection<Book> Books { get; set; }
