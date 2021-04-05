0cc365017a68daa33bdc11c64c29a59774ab27a3
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-05 22:00:51 +0400

    Create Rate.cs

---
+++ Domain/Entities/Rate.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Commons;
+
+namespace Domain.Entities
+{
+    public class Rate : AuditEntity
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string IconUrl { get; set; }
+    }
+}
