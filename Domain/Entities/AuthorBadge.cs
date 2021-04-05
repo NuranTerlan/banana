f0f984a5c5c45b6d999770706910316cd4cd4a52
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-05 22:06:45 +0400

    Create AuthorBadge.cs

---
+++ Domain/Entities/AuthorBadge.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities
+{
+    public class AuthorBadge
+    {
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+
+        public int BadgeId { get; set; }
+        public Badge Badge { get; set; }
+    }
+}
