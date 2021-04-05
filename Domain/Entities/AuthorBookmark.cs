8076b4082075d83cb6f791eaca87aa32991c5835
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-05 22:06:48 +0400

    Create AuthorBookmark.cs

---
+++ Domain/Entities/AuthorBookmark.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities
+{
+    public class AuthorBookmark
+    {
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+    }
+}
