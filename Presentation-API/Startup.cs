2d54ca4b3691ac0f1f94d0ac4b1d81ced8cee59c
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-06 22:06:23 +0400

    Update Startup.cs

--- Presentation-API/Startup.cs
+++ Presentation-API/Startup.cs
@@ -1,13 +1,11 @@
 using System;
+using Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using Presentation_API.Data;
 
 namespace Presentation_API
 {
@@ -23,6 +21,8 @@ namespace Presentation_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddInfrastructure(Configuration);
+
             services.AddControllersWithViews();
 
             services.AddSwaggerGen(options =>
