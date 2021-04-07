b78e9b80956d8907adc2b7f31993d7fc7c70875c
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-07 23:58:59 +0400

    Create DependencyInjection.cs

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Application.Books.Queries;
+using Application.Commons.Behaviors;
+using AutoMapper;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(GetAllBooksQuery));
+            services.AddTransient(typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>));
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
