34b1e1d4231676c63dc0a526895118f34f8c00dc
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-07 23:56:59 +0400

    Create BookController.cs

---
+++ Presentation-API/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Application.Books.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation_API.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public BookController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("api/books")]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _mediator.Send(new GetAllBooksQuery()));
+        }
+    }
+}
