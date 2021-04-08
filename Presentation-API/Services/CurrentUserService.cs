a4a52c28f457edecc022f3c89ee44578c736d3cd
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-08 22:45:25 +0400

    Create CurrentUserService.cs

---
+++ Presentation-API/Services/CurrentUserService.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+using Application.Commons.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace Presentation_API.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        public string UserId { get; }
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            UserId = httpContextAccessor.HttpContext?.User?
+                .FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
