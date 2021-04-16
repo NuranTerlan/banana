cc1f4e5d984177502417a1c005d1bed2d276752b
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-16 23:20:09 +0400

    Create ReaderProgress.cs

---
+++ Domain/Entities/ReaderProgress.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Entities
+{
+    public class ReaderProgress
+    {
+        public bool IsCompleted { get; set; }
+        public int CountToComplete { get; set; }
+        public int PagesPerDay { get; set; }
+        public int AveragePerformance { get; set; }
+        public int LastDayPagesCount { get; set; }
+
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+
+        public string AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
